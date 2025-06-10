277fa8cb4cdede30d76858e287a971c29da00aba
Tasya Putri Destianti <rekhahjrn@gmail.com>
2025-06-10 11:59:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("kisah kasih di sekolah")
 st.write(
-  "menceritakan tentang cinta yang muncul di lingkungan sekolah, di mana perasaan itu tumbuh di antara siswa-siswi yang saling berinteraksi dalam kegiatan belajar mengajar dan kegiatan ekstrakurikuler"
-    (
+ "menceritakan tentang cinta yang muncul di lingkungan sekolah, di mana perasaan itu tumbuh di antara siswa-siswi yang saling berinteraksi dalam kegiatan belajar mengajar dan kegiatan ekstrakurikuler"
+) 
